ace9bf814e4ec029f0c0277df706f0bcbfd541d4
Hanz0Hatt0r1 <51112916+Hanz0Hatt0r1@users.noreply.github.com>
2026-09-23 15:53:02 +0300

    Test MEB vertex layout mapping

---
+++ tests/test_vertex_layout.py
@@ -0,0 +1,14 @@
+from vertex_layout import build_vertex_layout
+
+def test_known_float_and_skin_attributes():
+    r=build_vertex_layout(["200","220","130","310","580"])
+    a={x["property_id"]:x for x in r["attributes"]}
+    assert a["200"]["android"]=="FLOAT32x3"
+    assert (a["310"]["usage"],a["310"]["usage_index"])==("BLENDWEIGHT",0)
+    assert a["580"]["android"]=="UINT8x4"
+
+def test_color_keeps_d3d9_ambiguity():
+    r=build_vertex_layout(["460"])
+    a=r["attributes"][0]
+    assert set(a["d3d9_candidates"])=={"D3DCOLOR","UBYTE4N"}
+    assert a["normalized"] is True
